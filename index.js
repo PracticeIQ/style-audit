89042170612b2a71deccf06b02108dc87673f290
I <tamara@balancedlabs.com>
2015-06-29 12:03:38 +1000

    Add some command line options to specify the filename

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var Color = require("./libs/color");
-var args = process.argv;
+var args = require('minimist')(process.argv.slice(2));
 var express = require("express");
 var app = express();
 
@@ -10,17 +10,28 @@ app.use(express.static("public"));
 var testFileName = "test/style.css";
 
 // Test
-var test = function() {
-  var color = new Color(testFileName);
+var getColorGuardReport = function(filename) {
+  var useName = filename;
+  if (!filename) {
+    useName = testFileName;
+  }
+  var color = new Color(useName);
   var output = color.inspect();
   return output;
 };
 
 
 app.get('/', function(req, res) {
-  var output = test();
-  output.fileName = testFileName;
-  console.log(output);
+
+  var output = getColorGuardReport();
+
+  if (args.filename) {
+    console.log("Running a report for " + args.filename + ", check it out on http://localhost:8888");
+    output = getColorGuardReport(args.filename);
+  }
+
+  output.fileName = args.filename || testFileName;
+
   res.render("index", output);
 });
 
